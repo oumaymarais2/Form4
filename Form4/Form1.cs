0a8f2a7a02ae4e4b0f4a923529c39e9ae6d90bca
oumaymarais2 <116873085+oumaymarais2@users.noreply.github.com>
2022-11-30 22:32:43 +0100

    Update Form1.cs

--- Form4/Form1.cs
+++ Form4/Form1.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,15 +21,23 @@ namespace Form4
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			SqlConnection con = new SqlConnection("Data Source=DESKTOP-64KR7SG\\SQLEXPRESS;Initial Catalog=Books;Integrated Security=True");
+			SqlConnection con = new SqlConnection("Data Source=DESKTOP-64KR7SG\\SQLEXPRESS;Initial Catalog=livre;Integrated Security=True");
 			con.Open();
-			SqlCommand cmd = new SqlCommand("insert into Library values(@ID,@Name,@Price)", con);
-			cmd.Parameters.AddWithValue("@ID",int.Parse(textBox1.Text));
+			SqlCommand cmd = new SqlCommand("insert into book values('" + int.Parse(textBox1.Text) + "', '" + textBox2.Text + "', '" + int.Parse(textBox3.Text) + "')", con);
+			/*cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
 			cmd.Parameters.AddWithValue("@Name", textBox2.Text);
-			cmd.Parameters.AddWithValue("@Price", int.Parse(textBox3.Text));
-			
+			cmd.Parameters.AddWithValue("@Price", int.Parse(textBox3.Text));*/
+			 MessageBox.Show(cmd.CommandText);
+			try { 
+				cmd.ExecuteNonQuery();
+				MessageBox.Show("seccesfully insert");
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
 			con.Close();
-			MessageBox.Show("seccesfully insert");
+			
 
 
 
@@ -36,29 +45,43 @@ namespace Form4
 
 		private void button2_Click(object sender, EventArgs e)
 		{
-			SqlConnection con = new SqlConnection("Data Source=DESKTOP-64KR7SG\\SQLEXPRESS;Initial Catalog=Books;Integrated Security=True");
+			SqlConnection con = new SqlConnection("Data Source=DESKTOP-64KR7SG\\SQLEXPRESS;Initial Catalog=livre;Integrated Security=True");
 			con.Open();
-			SqlCommand cmd = new SqlCommand("Update Library set Name=@Name,Price=@Price where ID=@ID",con);
-			cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
-			cmd.Parameters.AddWithValue("@Name", textBox2.Text);
-			cmd.Parameters.AddWithValue("@Price", int.Parse(textBox3.Text));
-			cmd.ExecuteNonQuery();
+			SqlCommand cmd = new SqlCommand("update book set Name='" + textBox2.Text + "',Prise= '" + int.Parse(textBox3.Text) + "' where ID='" + int.Parse(textBox1.Text) + "'", con);
+			
+			try { 
+				cmd.ExecuteNonQuery();
+				MessageBox.Show("seccesfully update");
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
 			con.Close();
-			MessageBox.Show("seccesfully updated");
 		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
-			SqlConnection con = new SqlConnection("Data Source=DESKTOP-64KR7SG\\SQLEXPRESS;Initial Catalog=Books;Integrated Security=True");
+			SqlConnection con = new SqlConnection("Data Source=DESKTOP-64KR7SG\\SQLEXPRESS;Initial Catalog=livre;Integrated Security=True");
 			con.Open();
-			SqlCommand cmd = new SqlCommand("Delete from Library  where ID=@ID", con);
-			cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+			SqlCommand cmd = new SqlCommand("Delete from book  where ID='" + int.Parse(textBox1.Text) + "'", con);
+			
+			try
+			{
+				cmd.ExecuteNonQuery();
+				MessageBox.Show("seccesfully delete");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
 			con.Close();
-			MessageBox.Show("seccesfully Deleted");
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
+			// TODO: This line of code loads data into the 'livreDataSet.book' table. You can move, or remove it, as needed.
+			this.bookTableAdapter.Fill(this.livreDataSet.book);
 			// TODO: This line of code loads data into the 'booksDataSet.Library' table. You can move, or remove it, as needed.
 			this.libraryTableAdapter.Fill(this.booksDataSet.Library);
 
